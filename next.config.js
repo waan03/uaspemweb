8efbb496990f7cbe55852a7debda79df82be118c
Despoka <nbilfrz04@gmail.com>
2023-12-22 01:52:22 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
     images: {
-        unoptimized: true,
         remotePatterns: [
-            
             {
                 hostname: "cdn.myanimelist.net"
             },
