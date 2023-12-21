60a91615fccf930704fdd4c3b64a374e1a4b08db
Dzakwan Rauhillah <88754182+waan03@users.noreply.github.com>
2023-12-21 11:30:09 +0700

    404 done

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,26 +1,26 @@
-import "./globals.css";
-import Image from 'next/image'
-import React from 'react'
- 
+import Link from "next/link";
+
 export default function NotFound() {
- 
   return (
-    <>
-      <div className="error-page w-full bg-gradient-radial from-SecondaryColor to-PrimaryColor h-screen">
-          <nav className='w-full px-10 py-10'>
-            
-          </nav>
-
-          <div className="error-box flex flex-col justify-between items-center h-[80%]">
-            <div className="not-found-box text-center">
-              <h1 className="text-[15rem] leading-none font-bold text-white">404</h1>
-              <p className="light text-2xl text-white mt-5 tracking-[0.3em]">PAGE NOT FOUND</p>
-            </div>
-
-            <Image src="/assets/images/HeroLogo.png" width={300} height={300} alt='Logo' />
-
-          </div>
+    <div className="bg-white items-center justify-center h-screen flex static flex-col ">
+      <h1 className="text-[15rem] leading-none font-bold text-[#15141F] pb-24">404</h1>
+      <div className="text-black flex">
+        <h2>Not Found</h2>
       </div>
-  </>
-  )
+      <div className="text-black flex">
+        <p>Could not find requested resource</p>
+      </div>
+      <img
+        src="/assets/images/404/404.png"
+        width={0}
+        height={0}
+        sizes=""
+        style={{ width: "20%", height: "30%" }}
+        alt="404"
+      />
+      <div className="border-black border rounded-lg bg-black p-2 px-8">
+        <Link href="/">Return Home</Link>
+      </div>
+    </div>
+  );
 }
